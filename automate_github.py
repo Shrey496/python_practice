dd4efbd535ad5930e7e6ba50518e57c674413552
Shrey496 <shreyr7@gmail.com>
2023-06-24 18:15:32 -0500

    automate_github project/code successfully completed

---
+++ automate_github.py
@@ -0,0 +1,45 @@
+import requests
+import json
+
+base_url = "https://api.github.com"
+token = "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"
+headers = {
+    "Authorization": f"Bearer {token}",
+    "Accept": "application/vnd.github.v3+json"
+}
+def list_repositories():
+    url = f"{base_url}/user/repos"
+    response = requests.get(url, headers=headers)
+    repositories = json.loads(response.text)
+    for repository in repositories:
+        print(repository["name"])
+
+def create_repository(repo_name):
+    url = f"{base_url}/user/repos"
+    data = {
+        "name": repo_name,
+        "auto_init": True
+    }
+    response = requests.post(url, headers=headers, json=data)
+    if response.status_code == 201:
+        print(f"Repository '{repo_name}' created successfully.")
+    else:
+        print("Failed to create repository.")
+
+def delete_repository(repo_name):
+    url = f"{base_url}/repos/Shrey496/{repo_name}"
+    response = requests.delete(url, headers=headers)
+    if response.status_code == 204:
+        print(f"Repository '{repo_name}' deleted successfully. ")
+    else:
+        print("Failed to delete repository.")
+
+
+# List repositories
+list_repositories()
+
+# Create a repository
+#create_repository("Created_by_code")
+
+# Delete a repository
+delete_repository("Created_by_code")
